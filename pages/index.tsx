fc8cbba5f4240ec47bb62c13f2aedf4484ee010f
Erick Rincones <erick.rincones@ciens.ucv.ve>
2020-11-03 10:44:14 -0400

    Improve template

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,9 @@ const Home = (): JSX.Element => {
     <>
       <SEO title="Moo!" />
 
-      <div className="flex flex-col md:flex-row-reverse w-screen">
+      <div className="flex flex-col md:flex-row-reverse md:min-h-screen">
         {/* Controls */}
-        <div className="flex flex-col p-2 w-full md:w-5/12 md:h-screen">
+        <div className="flex flex-col p-2 w-full md:w-5/12 min-h-full">
           {/* Options */}
           <div className="leading-none">
             <div className="flex mb-2">
@@ -30,15 +30,15 @@ const Home = (): JSX.Element => {
                 <legend className="cursor-default px-1">Action</legend>
                 <div className="flex">
                   <div className="bg-transparent pr-2 w-3/7">
-                    <input id="say" type="radio" checked={true} className="hidden" />
+                    <input id="say" type="radio" name="action" value="say" className="hidden" />
                     <label htmlFor="say">
-                      <span className="cursor-pointer">(<span tabIndex={0} className="whitespace-pre focus:bg-white focus:text-black">{`*`}</span>)</span>Say
+                      <span className="cursor-pointer">(<span tabIndex={0} className="whitespace-pre focus:bg-white focus:text-black focus:outline-none">{`*`}</span>)</span>Say
                     </label>
                   </div>
                   <div className="bg-transparent ml-4 w-4/7">
-                    <input id="think" type="radio" checked={false} className="hidden" />
+                    <input id="think" type="radio" name="action" value="think" className="hidden" />
                     <label htmlFor="think">
-                      <span className="cursor-pointer">(<span tabIndex={0} className="whitespace-pre focus:bg-white focus:text-black">{` `}</span>)</span>Think
+                      <span className="cursor-pointer">(<span tabIndex={0} className="whitespace-pre focus:bg-white focus:text-black focus:outline-none">{` `}</span>)</span>Think
                     </label>
                   </div>
                 </div>
@@ -62,7 +62,7 @@ const Home = (): JSX.Element => {
                   <legend className="cursor-default px-1">
                     <label htmlFor="eyes">Eyes</label>
                   </legend>
-                  <input id="eyes" type="text" value="oo" maxLength={2} autoCapitalize="off" spellCheck={false} className="bg-transparent text-white focus:outline-none w-full" />
+                  <input id="eyes" type="text" value="oo" maxLength={2} autoCapitalize="none" spellCheck={false} className="bg-transparent text-white focus:outline-none w-full" />
                 </fieldset>
 
                 {/* Eyes */}
@@ -70,7 +70,7 @@ const Home = (): JSX.Element => {
                   <legend className="cursor-default px-1">
                     <label htmlFor="tongue">Tongue</label>
                   </legend>
-                  <input id="tongue" type="text" value="" maxLength={2} autoCapitalize="off" spellCheck={false} className="bg-transparent text-white focus:outline-none w-full" />
+                  <input id="tongue" type="text" value="" maxLength={2} autoCapitalize="none" spellCheck={false} className="bg-transparent text-white focus:outline-none w-full" />
                 </fieldset>
               </div>
             </div>
@@ -86,7 +86,7 @@ const Home = (): JSX.Element => {
                   <div className="bg-transparent pl-2 ml-4 w-7/12">
                     <input id="no-wrap" type="checkbox" checked={false} className="hidden" />
                     <label htmlFor="no-wrap">
-                      <span className="cursor-pointer">[<span tabIndex={0} className="whitespace-pre focus:bg-white focus:text-black">{` `}</span>]</span>No wrap
+                      <span className="cursor-pointer">[<span tabIndex={0} className="whitespace-pre focus:bg-white focus:text-black focus:outline-none">{` `}</span>]</span>No wrap
                     </label>
                   </div>
                 </div>
@@ -100,42 +100,45 @@ const Home = (): JSX.Element => {
               <legend className="cursor-default px-1">
                 <label htmlFor="message">Message</label>
               </legend>
-              <textarea id="message" value="moo!" autoCapitalize="off" spellCheck={false} className="bg-black text-white focus:outline-none w-full min-h-10 md:min-h-full resize-y md:resize-none" />
+              <textarea id="message" value="moo!" autoCapitalize="none" spellCheck={false} autoFocus className="bg-black text-white focus:outline-none w-full min-h-10 md:min-h-full resize-y md:resize-none" />
             </fieldset>
           </div>
         </div>
 
         {/* Terminal */}
-        <div className="break-all whitespace-pre-wrap cursor-text px-px w-full md:w-7/12">
-          <pre className="overflow-x-auto">{` ______\n< moo! >\n ------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||\n`}</pre>
-          <div>
-            <pre><strong><span className="text-green-light">[user@localhost</span> moo<span className="text-green-light">]$</span> </strong>help</pre>
-            <pre>Moo! Developed by Erick Rincones.</pre>
-            <pre>Special thanks to Aury Rincones.</pre>
-            <pre>Licensed under the <a href="#" className="underline">MIT license</a>.</pre>
-            <pre>{`\n`}</pre>
-            <pre>These shell commands are defined internally. Type `help&apos; to see this list.</pre>
-            <pre>{`\n`}</pre>
-            <pre>
+        <div className="flex flex-col cursor-text px-px w-full md:w-7/12">
+          <pre className="select-all whitespace-pre overflow-x-auto">{` ______\n< moo! >\n ------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||\n`}</pre>
+          <div className="flex flex-col flex-grow">
+            <pre className="break-all whitespace-pre-wrap"><strong><span className="text-green-light">[user@localhost</span> moo<span className="text-green-light">]$</span> </strong>help</pre>
+            <pre className="break-all whitespace-pre-wrap">Moo! Developed by Erick Rincones.</pre>
+            <pre className="break-all whitespace-pre-wrap">Special thanks to Aury Rincones.</pre>
+            <pre className="break-all whitespace-pre-wrap">Licensed under the <a href="#" className="underline focus:outline-none">MIT license</a>.</pre>
+            <pre className="break-all whitespace-pre-wrap">{`\n`}</pre>
+            <pre className="break-all whitespace-pre-wrap">These shell commands are defined internally. Type `help&apos; to see this list.</pre>
+            <pre className="break-all whitespace-pre-wrap">{`\n`}</pre>
+            <pre className="break-all whitespace-pre-wrap">
               <ul className="columns-2 gap-0 w-max-content max-w-full">
-                <li className="pl-ch">clear</li>
-                <li className="pl-ch">echo [STRING]</li>
-                <li className="pl-ch">help</li>
-                <li className="pl-ch">history [-c]</li>
-                <li className="pl-ch">ls</li>
-                <li className="pl-ch">sudo [COMMAND]</li>
+                <li className="truncate pl-ch">clear</li>
+                <li className="truncate pl-ch">echo [STRING]</li>
+                <li className="truncate pl-ch">help</li>
+                <li className="truncate pl-ch">history [-c]</li>
+                <li className="truncate pl-ch">ls</li>
+                <li className="truncate pl-ch">sudo [COMMAND]</li>
               </ul>
             </pre>
 
-            <pre><strong><span className="text-green-light">[user@localhost</span> moo<span className="text-green-light">]$</span> </strong>sudo su</pre>
+            <pre className="break-all whitespace-pre-wrap"><strong><span className="text-green-light">[user@localhost</span> moo<span className="text-green-light">]$</span> </strong>sudo su</pre>
 
-            <pre><strong><span className="text-red-light">[localhost</span><span className="text-cyan-light"> moo</span><span className="text-red-light">]#</span> </strong>exit</pre>
-            <pre>exit</pre>
+            <pre className="break-all whitespace-pre-wrap"><strong><span className="text-red-light">[localhost</span><span className="text-cyan-light"> moo</span><span className="text-red-light">]#</span> </strong>exit</pre>
+            <pre className="break-all whitespace-pre-wrap">exit</pre>
 
-            <pre><strong><span className="text-green-light">[user@localhost</span> moo<span className="text-green-light">]$</span> </strong>cd here</pre>
-            <pre>moo!: cd: command not found</pre>
+            <pre className="break-all whitespace-pre-wrap"><strong><span className="text-green-light">[user@localhost</span> moo<span className="text-green-light">]$</span> </strong>cd here</pre>
+            <pre className="break-all whitespace-pre-wrap">moo!: cd: command not found</pre>
 
-            <pre><strong><span className="text-green-light">[user@localhost</span> moo<span className="text-green-light">]$</span> </strong></pre>
+            <div className="flex-grow">
+              <pre className="inline break-all whitespace-pre-wrap"><strong><span className="text-green-light">[user@localhost</span> moo<span className="text-green-light">]$</span> </strong></pre>
+              <pre autoCapitalize="none" spellCheck={false} contentEditable tabIndex={0} className="inline bg-black text-white break-all whitespace-pre-wrap focus:outline-none w-full" />
+            </div>
           </div>
         </div>
       </div>
