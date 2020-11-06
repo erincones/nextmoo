7308e0cfcb2eb721b0b8def0ca623eff82dad2ff
Erick Rincones <erick.rincones@ciens.ucv.ve>
2020-11-06 03:10:13 -0400

    Setup Open Graph and Twitter Card image

--- components/seo.tsx
+++ components/seo.tsx
@@ -25,7 +25,7 @@ export const SEO = ({ title, twitterCard = true, openGraph = true }: SEOProps): 
           <meta name="twitter:title" content="Moo!" />
           <meta name="twitter:description" content="Another web version of cowsay powered by Next.js" />
           <meta name="twitter:creator" content="@ErickRincones" />
-          <meta name="twitter:image" content="" />
+          <meta name="twitter:image" content="https://nextmoo.vercel.app/cover.png" />
         </>
       }
 
@@ -34,8 +34,8 @@ export const SEO = ({ title, twitterCard = true, openGraph = true }: SEOProps): 
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Moo!" />
           <meta property="og:description" content="Another web version of cowsay powered by Next.js" />
-          <meta property="og:url" content="" />
-          <meta property="og:image" content="" />
+          <meta property="og:url" content="https://nextmoo.vercel.app" />
+          <meta property="og:image" content="https://nextmoo.vercel.app/cover.png" />
           <meta property="og:image:width" content="1920" />
           <meta property="og:image:height" content="1080" />
         </>
